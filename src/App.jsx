e9c28cc472bc4b5f87314d3e6598e886a9ee3a1a
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-12-14 19:01:25 +0530

    Added Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './features/authSlice'
+import { Header, Footer } from './components/index'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
-  return (
-    <>
-      <h1>A Blog with Appwrite</h1>
-    </>
+
+
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => {
+        return setLoading(false)
+      })
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
   )
 }
 
